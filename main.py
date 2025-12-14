9c05c3a8910a2be598523e3c26ea3a6c71511981
KeaganBaller <kmhansen100@gmail.com>
2025-12-13 23:28:59 -0600

    second commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from logic import *
+import sys
 def main():
     application = QApplication([])
     window = Logic()
@@ -7,4 +8,8 @@ def main():
 
 
 if __name__=="__main__":
-    main()
+    try:
+        main()
+    except Exception as e:
+        print(f"Error running program: {e}")
+        sys.exit(0)
